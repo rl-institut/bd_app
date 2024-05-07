74ecaddf75ca044ef5b8e5a895fdbae002055035
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-05-07 13:37:05 +0200

    Initial commit

---
+++ building_dialouge_webapp/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from building_dialouge_webapp.users.models import User
+from building_dialouge_webapp.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
---
+++ building_dialouge_webapp/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from building_dialouge_webapp.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
