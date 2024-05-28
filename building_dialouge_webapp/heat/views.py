2fd161d5af0d670e1abb7703f2c3344a94f087c1
josihoppe <116898820+josihoppe@users.noreply.github.com>
2024-05-28 14:09:43 +0200

    added view logic to submit and validate forms

--- building_dialouge_webapp/heat/views.py
+++ building_dialouge_webapp/heat/views.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import *  # noqa: F403
@@ -21,8 +22,21 @@ def load_all_forms(request):
         if isinstance(f, type) and issubclass(f, forms.Form)
     ]
 
-    context = {
-        "form_instances": [f() for f in form_classes],
-    }
+    if request.method == "POST":
+        # Instantiate form instances with submitted data
+        form_instances = [f(request.POST) for f in form_classes]
+        all_valid = all(form.is_valid() for form in form_instances)
+
+        context = {"form_instances": form_instances}
+        if all_valid:
+            for form in form_instances:
+                print(form.cleaned_data)  # noqa: T201
+                # logic to handle form data
+            return HttpResponse("All forms are valid and have been processed.")
+
+    else:
+        context = {
+            "form_instances": [f() for f in form_classes],
+        }
 
     return render(request, "pages/heat_forms.html", context)
