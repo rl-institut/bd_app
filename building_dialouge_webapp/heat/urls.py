70c095a8531dc038bab22aa1d682f83710128371
josihoppe <116898820+josihoppe@users.noreply.github.com>
2024-11-15 17:03:28 +0100

    removed old forms and view, renamed usage to consumption, structured urls

--- building_dialouge_webapp/heat/urls.py
+++ building_dialouge_webapp/heat/urls.py
@@ -6,11 +6,14 @@ from . import views
 app_name = "heat"
 
 urlpatterns = [
-    path("forms/", views.handle_forms, name="forms"),
-    path("roof/", flows.RoofFlow.as_view(), name="roof"),
-    path("cellar/", flows.CellarFlow.as_view(), name="cellar"),
+    # sorted by appearance in view flow
     path("", views.LandingPage.as_view(), name="home"),
-    path("intro_usage/", views.IntroUsage.as_view(), name="intro_usage"),
+    # step 1 consumption analysis
+    path("intro_consumption/", views.IntroConsumption.as_view(), name="intro_consumption"),
+    path("cellar/", flows.CellarFlow.as_view(), name="cellar"),
+    # step 2 inventory analysis
     path("intro_inventory/", views.IntroInventory.as_view(), name="intro_inventory"),
+    path("roof/", flows.RoofFlow.as_view(), name="roof"),
+    # step 3 renovation request
     path("intro_renovation/", views.IntroRenovation.as_view(), name="intro_renovation"),
 ]
