7943c538fb549589d74dada814a11d91e388289d
josihoppe <116898820+josihoppe@users.noreply.github.com>
2024-12-04 13:55:23 +0100

    minor change

--- building_dialouge_webapp/heat/flows.py
+++ building_dialouge_webapp/heat/flows.py
@@ -519,7 +519,7 @@ class BuildingTypeFlow(SidebarNavigationMixin, Flow):
         ).transition(
             Switch("monument_protection").case("yes", "dead_end_monument_protection").default("end"),
         )
-        # TODO: make dead end popup or view and update url
+
         self.dead_end_monument_protection = EndState(self, url="heat:dead_end_monument_protection")
 
         self.end = EndState(self, url="heat:building_data")
