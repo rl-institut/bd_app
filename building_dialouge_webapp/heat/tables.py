8098d19ee75f004c036645ec2cce963e67a507b0
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-01-14 16:50:23 +0100

    Refactor SumTable and generation of table data

--- building_dialouge_webapp/heat/tables.py
+++ building_dialouge_webapp/heat/tables.py
@@ -1,9 +1,10 @@
 from math import isclose
+from typing import Any
 
 import pandas as pd
 
 
-def format_currency(value):
+def format_currency(value: Any) -> str:
     """Format a number as currency (Euro)."""
     if value is None:
         return ""
@@ -26,14 +27,14 @@ class Table:
     def __init__(self, data):
         self.data = data
 
-    def translate_column(self, column_name):
+    def translate_column(self, column_name: str) -> str:
         return self.translations.get(column_name, column_name)
 
-    def generate_table_data(self):
-        return self.data
+    def generate_table_data(self) -> pd.DataFrame:
+        return pd.DataFrame(self.data)
 
-    def to_html(self, title):
-        dataframe = pd.DataFrame(self.generate_table_data())
+    def to_html(self, title: str) -> str:
+        dataframe = self.generate_table_data()
         html = dataframe.to_html(classes="table", escape=False, index=False)
 
         for header in dataframe.columns:
@@ -45,27 +46,6 @@ class Table:
                 )
 
         return html.replace('<table border="1" class="dataframe table">', f'<table class="{title} table">')
-
-
-class SumTable(Table):
-    def generate_sum_row(self, table_data):
-        dataframe = pd.DataFrame(table_data)
-        if not dataframe.empty and dataframe.shape[1] > 1:
-            sum_row = ["Summe"]
-            for col in dataframe.columns[1:]:
-                numeric_series = pd.to_numeric(dataframe[col], errors="coerce")
-                col_sum = numeric_series.sum(skipna=True)
-                sum_row.append(col_sum)
-
-            dataframe.loc[len(dataframe)] = sum_row
-
-            for col in dataframe.columns[1:]:
-                dataframe[col] = dataframe[col].apply(format_currency)
-
-        return dataframe
-
-    def to_html(self, title="sum_table"):
-        return super().to_html(title)
 
 
 class ConsumptionTable(Table):
@@ -87,7 +67,7 @@ class ConsumptionTable(Table):
         "insulate_basement_ceiling",
     ]
 
-    def generate_table_data(self):
+    def generate_table_data(self) -> pd.DataFrame:
         table_data = {
             self.translate_column("procedure"): [self.translations.get(proc, proc) for proc in self.procedures],
         }
@@ -95,14 +75,39 @@ class ConsumptionTable(Table):
             table_data[self.translate_column(scenario)] = [
                 scenario_data.get(proc, "nicht ausgewählt") for proc in self.procedures
             ]
-        return table_data
+        return pd.DataFrame(table_data)
 
-    def to_html(self, title="consumption_table"):
+    def to_html(self, title: str = "consumption_table") -> str:
         html_table = super().to_html(title)
         return html_table.replace(
             "<td>nicht ausgewählt</td>",
             '<td class="no-entry">nicht ausgewählt</td>',
         )
+
+
+class SumTable(Table):
+    @staticmethod
+    def generate_sum_row(dataframe: pd.DataFrame) -> pd.DataFrame:
+        if not dataframe.empty and dataframe.shape[1] > 1:
+            sum_row = ["Summe"]
+            for col in dataframe.columns[1:]:
+                numeric_series = pd.to_numeric(dataframe[col], errors="coerce")
+                col_sum = numeric_series.sum(skipna=True)
+                sum_row.append(col_sum)
+
+            dataframe.loc[len(dataframe)] = sum_row
+
+            for col in dataframe.columns[1:]:
+                dataframe[col] = dataframe[col].apply(format_currency)
+
+        return dataframe
+
+    def generate_table_data(self):
+        dataframe = super().generate_table_data()
+        return self.generate_sum_row(dataframe)
+
+    def to_html(self, title: str = "sum_table"):
+        return super().to_html(title)
 
 
 class InvestmentTable(SumTable):
@@ -111,7 +116,7 @@ class InvestmentTable(SumTable):
         "investment_overview": "Finanzierungsübersicht",
     }
 
-    def generate_table_data(self):
+    def generate_table_data(self) -> pd.DataFrame:
         table_data = {
             self.translate_column("investment_overview"): [
                 "Investitionskosten",
@@ -123,9 +128,9 @@ class InvestmentTable(SumTable):
                 scenario_data.get("investment", 0),
                 scenario_data.get("contribution", 0),
             ]
-        return self.generate_sum_row(table_data)
+        return self.generate_sum_row(pd.DataFrame(table_data))
 
-    def to_html(self, title="investment_table"):
+    def to_html(self, title: str = "investment_table") -> str:
         return super().to_html(title)
 
 
