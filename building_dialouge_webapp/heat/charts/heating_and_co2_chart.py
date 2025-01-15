00aae649e4bbf4c98ee14ca1cf702ffed5f92c1c
sarah <sarah.sommer@rl-institut.de>
2025-01-15 07:57:59 +0100

    added investment_costs_chart.py, changed heating_consumption: added co2-chart to heating_chart

--- building_dialouge_webapp/heat/charts/heating_and_co2_chart.py
+++ building_dialouge_webapp/heat/charts/heating_and_co2_chart.py
@@ -1,7 +1,6 @@
 import json
 
-
-def generate_echarts_option(scenarios):
+def generate_echarts_option(scenarios, title):
     default_colors = {
         "Heute": "#dcdcdb",
         "Szenario 1": "#1b9e77",
@@ -14,7 +13,10 @@ def generate_echarts_option(scenarios):
         "Szenario 2": "white",
     }
 
-    y_axis_data = [s["name"] for s in reversed(scenarios)]
+    order = {"Heute": 1, "Szenario 1": 2, "Szenario 2": 3, "Szenario 3": 4}
+    sorted_scenarios = sorted(scenarios, key=lambda x: order.get(x["name"], 5))
+
+    y_axis_data = [s["name"] for s in reversed(sorted_scenarios)]
 
     series_data = []
     for s in reversed(scenarios):
@@ -36,14 +38,21 @@ def generate_echarts_option(scenarios):
             },
         )
 
-    font_size = 24 if len(scenarios) == 4 else (40 if len(scenarios) == 2 else 34)
+    title_top = "55%"
+    grid_height = 200
+    rotate = 0
+    font_size = 24 if len(scenarios) == 4 else (44 if len(scenarios) == 2 else 34)
+    if title == "CO2-Kosten in € pro Jahr":
+        rotate = 45
+        grid_height = 250
+        title_top = "50%"
 
     option = {
         "grid": {
             "left": "20%",
             "right": "4%",
             "bottom": "3%",
-            "height": 200,
+            "height": grid_height,
             "containLabel": True,
         },
         "xAxis": {
@@ -52,6 +61,7 @@ def generate_echarts_option(scenarios):
                 "show": True,
                 "fontSize": 26,
                 "color": "#b3b4b2",
+                "rotate": rotate
             },
             "splitLine": {
                 "show": True,
@@ -72,9 +82,9 @@ def generate_echarts_option(scenarios):
             },
         },
         "title": {
-            "text": "Heizenergieverbrauch in kWh/(m²*a) (Endenergie)",
+            "text": title,
             "left": "0%",
-            "top": "55%",
+            "top": title_top,
             "textStyle": {
                 "fontWeight": "normal",
                 "fontSize": 34,
@@ -100,24 +110,24 @@ def generate_echarts_option(scenarios):
     return option
 
 
-# Beispiele:
+# Beispiele heating_consumption:
 # 1) Nur ein Szenario
 single_scenario_option = generate_echarts_option(
     [
         {"name": "Heute", "value": 230},
         {"name": "Szenario 1", "value": 130},
-    ],
-)
+    ]
+,"Heizenergieverbrauch in kWh/(m²*a) (Endenergie)")
 print(json.dumps(single_scenario_option, indent=2, ensure_ascii=False))
 
-# 2) Zwei Szenarien
+#2) Zwei Szenarien
 # two_scenarios_option = generate_echarts_option(
 #     [
 #         {"name": "Heute", "value": 230},
 #         {"name": "Szenario 1", "value": 130},
 #         {"name": "Szenario 2", "value": 47}
 #     ]
-# )
+# ,"Heizenergieverbrauch in kWh/(m²*a) (Endenergie)")
 # print(json.dumps(two_scenarios_option, indent=2, ensure_ascii=False))
 
 # 3) Drei Szenarien
@@ -128,5 +138,30 @@ print(json.dumps(single_scenario_option, indent=2, ensure_ascii=False))
 #          {"name": "Szenario 2", "value": 47},
 #          {"name": "Szenario 3", "value": 87}
 #      ]
-# )
+# ,"Heizenergieverbrauch in kWh/(m²*a) (Endenergie)")
+# print(json.dumps(three_scenarios_option, indent=2, ensure_ascii=False))
+# Beispiele CO2-Kosten:
+single_scenario_option = generate_echarts_option(
+    [
+        {"name": "Heute", "value": 2600},
+        {"name": "Szenario 1", "value": 1200},
+    ]
+,"CO2-Kosten in € pro Jahr")
+print(json.dumps(single_scenario_option, indent=2, ensure_ascii=False))
+# two_scenarios_option = generate_echarts_option(
+#     [
+#         {"name": "Heute", "value": 2600},
+#         {"name": "Szenario 1", "value": 1200},
+#         {"name": "Szenario 2", "value": 700},
+#     ]
+# ,"CO2-Kosten in € pro Jahr")
+# print(json.dumps(two_scenarios_option, indent=2, ensure_ascii=False))
+# three_scenarios_option = generate_echarts_option(
+#     [
+#          {"name": "Heute", "value": 2600},
+#          {"name": "Szenario 1", "value": 1200},
+#          {"name": "Szenario 2", "value": 700},
+#          {"name": "Szenario 3", "value": 900}
+#      ]
+# ,"CO2-Kosten in € pro Jahr")
 # print(json.dumps(three_scenarios_option, indent=2, ensure_ascii=False))
