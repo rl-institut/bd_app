e976a0c34320953dc9ce0068f1cf0940a525a4c6
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-02-20 14:26:18 +0100

    Set up oemof hook for extracting flow data

--- building_dialouge_webapp/heat/apps.py
+++ building_dialouge_webapp/heat/apps.py
@@ -4,3 +4,16 @@ from django.apps import AppConfig
 class HeatConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "building_dialouge_webapp.heat"
+
+    def ready(self) -> None:
+        """Content in here is run when app is ready."""
+        # pylint: disable=C0415
+        from django_oemof import hooks
+
+        # pylint: disable=C0415
+        from building_dialouge_webapp.heat import hooks as bd_hooks
+
+        hooks.register_hook(
+            hooks.HookType.SETUP,
+            hooks.Hook(scenario=hooks.ALL_SCENARIOS, function=bd_hooks.read_flow_data),
+        )
