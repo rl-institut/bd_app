943a32e7b062bc8836ff7718a37c304630d4d510
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-03-17 15:07:07 +0100

    Increase keyup delay to 1000ms

--- building_dialouge_webapp/static/js/flow_validation.js
+++ building_dialouge_webapp/static/js/flow_validation.js
@@ -24,14 +24,3 @@ document.addEventListener("htmx:beforeRequest", (event) => {
     event.preventDefault(); // Stop the HTMX request
   }
 });
-
-// extra check for Checkboxes in RenovationRequest - RenovationDetailsForm
-/* document.addEventListener("htmx:beforeSend", function(event) {
-  let checkboxes = document.querySelectorAll('input[name="roof_renovation_details"]');
-  let hiddenInput = document.querySelector('input[name="roof_renovation_details_hidden"]');
-
-  let anyChecked = Array.from(checkboxes).some(cb => cb.checked);
-  if (!anyChecked) {
-      hiddenInput.value = "none";
-  }
-}); */
