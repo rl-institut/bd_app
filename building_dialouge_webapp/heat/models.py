4ff2b6097898ade092b6604eddbc028afce27a0d
josihoppe <116898820+josihoppe@users.noreply.github.com>
2025-03-24 20:20:21 +0100

    added photovoltaic model

--- building_dialouge_webapp/heat/models.py
+++ building_dialouge_webapp/heat/models.py
@@ -18,3 +18,17 @@ class Solarthermal(models.Model):
 
     def __str__(self):
         return f"Solarthermal ({self.type}, {self.temperature}, {self.elevation_angle}, {self.direction_angle})"
+
+
+class Photovoltaic(models.Model):
+    """Model to hold timeseries for different setups of photovoltaic component."""
+
+    elevation_angle = models.IntegerField()
+    direction_angle = models.IntegerField()
+    profile = ArrayField(models.FloatField())
+
+    class Meta:
+        unique_together = ("elevation_angle", "direction_angle")
+
+    def __str__(self):
+        return f"Photovoltaic ({self.elevation_angle}, {self.direction_angle})"
