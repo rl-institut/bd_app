09d526845b8416daf74c59460aa4ce8ff02d55de
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-11-14 08:55:57 +0100

    Revert "Remove database entry in production.py"
    This reverts commit c8951b2970cabccbe00c5f1c839dcf95f7cd43ce.

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,6 +8,7 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
 
 from .base import *  # noqa: F403
+from .base import DATABASES
 from .base import INSTALLED_APPS
 from .base import env
 
@@ -17,6 +18,10 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["rl-institut.de"])
+
+# DATABASES
+# ------------------------------------------------------------------------------
+DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
 
 # CACHES
 # ------------------------------------------------------------------------------
